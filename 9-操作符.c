017b3bbcae9ac432ed1c91c84b38fc4604d62b8b
Jason Bourne <118343288+wuyepingxs@users.noreply.github.com>
2022-12-25 13:52:47 +0800

    Update 9-操作符.c

--- 9-操作符.c
+++ 9-操作符.c
@@ -43,3 +43,57 @@ int main()
 		printf("%d\n", answer);
 		return 0;
 	}
+
+
+
+
+// 1 初始化 2逆置，不是逆序
+#define _CRT_SECURE_NO_WARNINGS 
+
+void init(int arr[], int sz)
+{
+	int i = 0;
+	for (i = 0; i < sz; i++)
+	{
+		arr[i] = 0;
+	}
+}
+
+void print(int*arr, int sz)//数组名是首元素地址，可以用指针接收:    int*arr
+{
+	int i = 0;
+	for (i = 0; i < sz; i++)
+	{
+		printf("%d ", arr[i]);
+	}
+	printf("\n");
+}
+
+void reverse(int arr[], int sz)
+
+{
+	int left = 0;
+	int right = sz - 1;
+	while (left < right)
+	{
+		int tmp = arr[left];
+		arr[left] = arr[right];
+		arr[right] = tmp;
+		left++;
+		right--;
+	}
+
+
+}
+
+int main()
+{
+	int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	print(arr, sz);//，打印数组各个元素，传参 数组名就是首元素地址，sz就是数组元素个数
+	reverse(arr, sz);//逆置 交换 位置而已
+	print(arr, sz);
+	init(arr, sz);//初始化 使数组各元素为0
+	print(arr, sz);//打印 初始化后的数组 各个元素
+	return 0;
+}
