a94207e069cf17e238200cdee149cecff3519661
Jason Bourne <118343288+wuyepingxs@users.noreply.github.com>
2023-01-15 23:20:05 +0800

    Update 14小测验.c

--- 14小测验.c
+++ 14小测验.c
@@ -143,3 +143,38 @@ int main()
 	}*/
 	return 0;
 }
+
+
+//写代码判断当前机器的字节序
+//int check_sys()
+//
+//{
+//	int a = 1;
+//
+//	char*p = (char*)&a;
+//	if (*p == 1)
+//		return 1;
+//	else
+//		return 0;
+//}
+int check_sys()
+{
+	int a = 1;
+	char*p = (char*)&a;//int *
+	return *p;//返回1 表示小端，返回0表示大端
+}
+
+int main()
+{
+	int ret = check_sys();
+
+	if (ret == 1)
+	{
+		printf("小端\n");
+	}
+	else
+	{
+		printf("大端\n");
+	}
+	return 0;
+}
